362954f71dd233647a08c91de6b66aea4028028a
Dylan Claywell <djclaywell@gmail.com>
2022-04-08 18:08:14 -0500

    Added the ability to add new tags in the DB

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames'
-import { createEffect, createSignal, For, Index, JSX } from 'solid-js'
+import { createEffect, createSignal, For, Index } from 'solid-js'
 import cloneDeep from 'lodash.clonedeep'
 
 import { useTheme } from '../../contexts/Theme'
@@ -7,7 +7,12 @@ import Switch from '../Switch'
 import { query, mutation } from '../../gql/client'
 import getTagsQuery from '@graphql/gql/getTags.graphql?raw'
 import updateTagMutation from '@graphql/gql/updateTag.graphql?raw'
-import { MutationUpdateTagArgs, Tag } from '../../generated/graphql'
+import createTagMutation from '@graphql/gql/createTag.graphql?raw'
+import {
+  MutationCreateTagArgs,
+  MutationUpdateTagArgs,
+  Tag,
+} from '../../generated/graphql'
 
 import styles from './Settings.module.css'
 import { debounce } from 'debounce'
@@ -17,17 +22,39 @@ interface Errors {
   color: string[]
 }
 
+interface SettingsTag extends Tag {
+  isNew: boolean
+}
+
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 15)
+}
+
+function generateRandomColor() {
+  const numbers: string[] = []
+  for (let i = 0; i < 6; i++) {
+    numbers.push(generateRandomNumber().toString(16))
+  }
+
+  return `#${numbers.join('')}`
+}
+
 export default function Settings() {
   const [getErrors, setErrors] = createSignal<Errors>({
     name: [],
     color: [],
   })
-  const [getTags, setTags] = createSignal<Tag[]>([])
+  const [getTags, setTags] = createSignal<SettingsTag[]>([])
   const [getThemeState, { setTheme }] = useTheme()
 
   createEffect(async () => {
     const response = await query<void, Tag[]>(getTagsQuery)
-    setTags(response.data.tags)
+    setTags(
+      response.data.tags.map((tag) => ({
+        ...tag,
+        isNew: false,
+      }))
+    )
   })
 
   const addError = (name: keyof Errors, id: string) => {
@@ -79,19 +106,37 @@ export default function Settings() {
         }
       )
 
-      tags.splice(tagIndex, 1, response.data.updateTag)
+      tags.splice(tagIndex, 1, { ...response.data.updateTag, isNew: false })
 
       setTags(tags)
     },
     300
   )
 
+  const addTagRow = async () => {
+    const response = await mutation<MutationCreateTagArgs, Tag>(
+      createTagMutation,
+      {
+        color: generateRandomColor(),
+        name: 'New Tag',
+      }
+    )
+
+    setTags([
+      ...getTags(),
+      {
+        ...response.data.createTag,
+        isNew: true,
+      },
+    ])
+  }
+
   return (
     <div className={styles['settings']}>
       <h1 className={styles['settings-heading']}>Settings</h1>
       <h2>Tags</h2>
       <div className={styles['settings-tag-table']}>
-        <For each={getTags()}>
+        <Index each={getTags()}>
           {(tag) => (
             <div
               className={classnames(styles['settings-tag-table-row'], {
@@ -104,50 +149,51 @@ export default function Settings() {
                   className={classnames(styles['settings-tag-table-input'], {
                     [styles['settings-tag-table-input-error']]: hasError(
                       'name',
-                      tag.id
+                      tag().id
                     ),
                   })}
                   onInput={(e) => {
-                    onChangeInput(tag.id, 'name', e.currentTarget.value)
+                    onChangeInput(tag().id, 'name', e.currentTarget.value)
                   }}
-                  value={tag.name}
+                  value={tag().name}
                 />
               </div>
               <div className={styles['settings-tag-color']}>
                 <div
                   className={styles['settings-tag-color-sample']}
-                  style={{ 'background-color': tag.color }}
+                  style={{ 'background-color': tag().color }}
                 />
                 <input
                   className={classnames(styles['settings-tag-table-input'], {
                     [styles['settings-tag-table-input-error']]: hasError(
                       'color',
-                      tag.id
+                      tag().id
                     ),
                   })}
                   onInput={(e) => {
                     onChangeInput(
-                      tag.id,
+                      tag().id,
                       'color',
                       e.currentTarget.value,
                       /^#[A-Za-z0-9]{6}$/
                     )
                   }}
-                  value={tag.color}
+                  value={tag().color}
                 />
               </div>
             </div>
           )}
-        </For>
-        <div
+        </Index>
+        <button
           className={classnames(styles['settings-tag-table-add-row'], {
             [styles['settings-tag-table-add-row-dark']]:
               getThemeState().theme === 'dark',
           })}
+          onClick={addTagRow}
         >
           Add tag
           <i className="fa-solid fa-plus" />
-        </div>
+        </button>
       </div>
       <div className={styles['settings-theme-container']}>
         <span>Light Theme</span>
