ac17cbbc0061ffd8e78dad1a24a6e0a205dd653e
Dylan Claywell <djclaywell@gmail.com>
2022-07-07 22:33:47 -0500

    Updated the mobile styling for the home page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,8 +3,6 @@ import { Route, Routes } from 'solid-app-router'
 import Downloads from './downloads'
 import Home from './home'
 
-import styles from './main.module.css'
-
 export default function Main() {
   return (
     <Routes>
